44707bfae2411e4d299716935c1cbe704a9b8264
Trong An <troandev@gmail.com>
2024-05-18 06:46:47 +0700

    change meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,21 +20,24 @@ export default function RootLayout({
                 <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
                 <meta name="google-site-verification" content="CHsqH1AurGSgE3fcLmEYeeawL8BLrJSOQEsHSzcYpZ0" />
                 <meta httpEquiv="content-language" content="vi" /> <meta content="width=device-width, initial-scale=1.0, user-scalable=yes" name="viewport" />
+                <meta name="description" content="Phim Mới chất lượng cao miễn phí. Xem phim hd VietSub. Phim thuyết minh chất lượng HD. Kho phi mới chuẩn nhanh online hay hấp dẫn." />{" "}
+                <meta property="og:locale" content="vi_VN" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Phim Mới | Phim HD | Xem phim nhanh | Phim VietSub | Thuyết Minh Hay Nhất" />
                 <meta
                     name="keywords"
-                    content="Phim Trung Quốc, Phim Hàn Quốc, Phim chiếu rạp, Phim hành động, Phim kinh di, Phim hài, Phim hoạt hình, Phim Mỹ, Phim Võ Thuật, Phim bộ hay nhất, Xem phim Online"
-                />{" "}
-                <meta name="description" content="Phim Mới chất lượng cao miễn phí. Xem phim hd VietSub. Phim thuyết minh chất lượng HD. Kho phi mới chuẩn nhanh online hay hấp dẫn." />{" "}
-                <meta name="ROBOTS" content="index,follow" /> <meta name="googlebot" content="index,follow" />
-                <meta name="BingBOT" content="index,follow" /> <meta name="yahooBOT" content="index,follow" /> <meta name="slurp" content="index,follow" />{" "}
-                <meta name="msnbot" content="index,follow" /> <meta name="theme-color" content="#ff8a00" /> <meta property="og:site_name" content="CaPhim" />
-                <meta property="og:locale" content="vi_VN" /> <meta property="og:title" content="Phim Mới | Phim HD | Xem phim nhanh | Phim VietSub | Thuyết Minh Hay Nhất" />
-                <meta property="og:description" content="Phim Mới chất lượng cao miễn phí. Xem phim hd VietSub. Phim thuyết minh chất lượng HD. Kho phim mới chuẩn nhanh online hay hấp dẫn." />
-                <meta property="og:type" content="website" /> <meta property="og:url" content="https://caphim.vercel.app/" />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:title" content="Phim Mới | Phim HD | Xem phim nhanh | Phim VietSub | Thuyết Minh Hay Nhất" />{" "}
-                <meta name="twitter:description" content="Phim Mới chất lượng cao miễn phí. Xem phim hd VietSub. Phim thuyết minh chất lượng HD. Kho phim mới chuẩn nhanh online hay hấp dẫn." />
-                <meta name="revisit-after" content="1 days" />
+                    content="Phim Trung Quốc, Phim Hàn Quốc, Phim chiếu rạp, Phim hành động, Phim kinh di, Phim hài, Phim hoạt hình, Phim Mỹ, Phim Võ Thuật, Phim bộ hay nhất, Xem phim Online, xem phim online tại caphim.vercel.app, caphim, cà phim, caphim, ca phim, xem phim hai tai caphim, doraemon caphim, phimmoi caphim, phim mới tại cà phim, phim chiếu rạp tại cà phim, phim lẻ cà phim, phim le caphim, phim bo ca phim, phim bộ tại cà phim, xem phim nhanh không quảng cáo, xem phim khong quang cao tai caphim"
+                />
+                <meta
+                    property="og:description"
+                    content="Phim Mới chất lượng cao miễn phí, không quảng cáo. Xem phim hd VietSub. Phim thuyết minh chất lượng HD. Kho phim mới chuẩn nhanh online hay hấp dẫn tại caphim"
+                />
+                <meta property="og:url" content="https://caphim.vercel.app/" />
+                <meta property="og:site_name" content="Cà Phim - Nền tảng coi phim trực không quảng cáo" />
+                <meta property="og:updated_time" content="2024-05-18T09:13:39+07:00" />
+                <meta property="og:image:alt" content="Cà Phim - Nền tảng coi phim trực không quảng cáo" />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="article:published_time" content="2024-05-17T09:13:39+07:00" />
             </head>
             <body className={inter.className} style={{ backgroundColor: "#09090b" }}>
                 <Header />
