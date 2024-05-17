c33c9b6381c915fa391146cd51a34c6176b8bd1f
Trong An <troandev@gmail.com>
2024-05-17 08:54:58 +0700

    search

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,19 +1,13 @@
 "use client";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Search, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Pacifico } from "next/font/google";
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
 
 const pacifico = Pacifico({ subsets: ["latin"], weight: "400" });
 
@@ -21,10 +15,44 @@ export default function Header() {
     const pathname = usePathname();
 
     const [hide, setHide] = useState(false);
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    function handleText(e: any) {
-        console.log(e.target.value);
+    function debounce(func: any, wait: any) {
+        let timeout: any;
+        return (...args: any) => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func(...args), wait);
+        };
     }
+
+    const handleText = (e: any) => {
+        const { value } = e.target;
+        setQuery(value);
+        handleSearch(value);
+    };
+
+    const handleSearch = useCallback(
+        debounce(async (value: any) => {
+            if (value) {
+                setLoading(true);
+                try {
+                    const res = await fetch(`https://phimapi.com/v1/api/tim-kiem?keyword=${value}&limit=7`);
+                    const data = await res.json();
+                    setResults(data.data.items);
+                    console.log(data.data.items);
+                } catch (error) {
+                    console.error("Error fetching search results: ", error);
+                } finally {
+                    setLoading(false);
+                }
+            } else {
+                setResults([]);
+            }
+        }, 300),
+        []
+    );
 
     return (
         <div className="block h-[60px] text-white relative z-10">
@@ -34,9 +62,7 @@ export default function Header() {
                         <Link href="/" className={pacifico.className}>
                             <div className="text-3xl flex gap-1">
                                 <h1 className="text-green-500">Cà </h1>
-                                <h1 className="text-yellow-400 font-bold">
-                                    Phim{" "}
-                                </h1>
+                                <h1 className="text-yellow-400 font-bold">Phim </h1>
                             </div>
                         </Link>
                         <div className="hidden md:block">
@@ -46,57 +72,27 @@ export default function Header() {
                                 ) : (
                                     <>
                                         <li className="">
-                                            <Link
-                                                href="/"
-                                                className={`${
-                                                    pathname === "/"
-                                                        ? "bg-orange-600"
-                                                        : ""
-                                                } block px-5 py-3`}>
+                                            <Link href="/" className={`${pathname === "/" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                 Trang chủ
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link
-                                                href="/phimbo"
-                                                className={`${
-                                                    pathname === "/phimbo"
-                                                        ? "bg-orange-600"
-                                                        : ""
-                                                } block px-5 py-3`}>
+                                            <Link href="/phimbo" className={`${pathname === "/phimbo" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                 Phim bộ
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link
-                                                href="/phimle"
-                                                className={`${
-                                                    pathname === "/phimle"
-                                                        ? "bg-orange-600"
-                                                        : ""
-                                                } block px-5 py-3`}>
+                                            <Link href="/phimle" className={`${pathname === "/phimle" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                 Phim lẻ
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link
-                                                href="/tvshow"
-                                                className={`${
-                                                    pathname === "/tvshow"
-                                                        ? "bg-orange-600"
-                                                        : ""
-                                                } block px-5 py-3`}>
+                                            <Link href="/tvshow" className={`${pathname === "/tvshow" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                 TV show
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link
-                                                href="/hoathinh"
-                                                className={`${
-                                                    pathname === "/hoathinh"
-                                                        ? "bg-orange-600"
-                                                        : ""
-                                                } block px-5 py-3`}>
+                                            <Link href="/hoathinh" className={`${pathname === "/hoathinh" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                 Hoạt hình
                                             </Link>
                                         </li>
@@ -104,13 +100,42 @@ export default function Header() {
                                 )}
                                 {hide ? (
                                     <>
-                                        <li className=" w-[600px]">
-                                            <Input
-                                                type="text"
-                                                placeholder="Nhập tên phim bạn muốn tìm..."
-                                                className=""
-                                                onChange={(e) => handleText(e)}
-                                            />
+                                        <li className=" w-[600px] relative">
+                                            <form action="">
+                                                <Input type="text" placeholder="Nhập tên phim bạn muốn tìm..." className="" onChange={(e) => handleText(e)} />
+                                            </form>
+                                            <div className="absolute w-[600px] bg-[#333333] mt-2 p-3 shadow-sm shadow-gray-500 ">
+                                                {loading ? (
+                                                    <p>Đang tìm kiếm...</p>
+                                                ) : (
+                                                    <div>
+                                                        {results ? (
+                                                            <div className="results-container ">
+                                                                <ul className="results-list flex flex-col">
+                                                                    {results.map((item: any, index) => (
+                                                                        <li key={index} className="h-[80px] result-item flex gap-3 items-center hover:text-green-500 cursor-pointer group">
+                                                                            <Image
+                                                                                src={`https://img.phimapi.com/${item.thumb_url}`}
+                                                                                width={100}
+                                                                                height={100}
+                                                                                alt=""
+                                                                                className="object-cover rounded-md "
+                                                                            />
+                                                                            <a href={`/phim/${item.slug}`} className="result-link ">
+                                                                                <p>{item.name}</p>
+                                                                                <p className="text-sm text-gray-500 group-hover:text-green-200">{item.origin_name}</p>
+                                                                                <p className="text-sm text-gray-500 group-hover:text-green-200">{item.year}</p>
+                                                                            </a>
+                                                                        </li>
+                                                                    ))}
+                                                                </ul>
+                                                            </div>
+                                                        ) : (
+                                                            <p>Không có kết quả...</p>
+                                                        )}
+                                                    </div>
+                                                )}
+                                            </div>
                                         </li>
                                     </>
                                 ) : (
@@ -119,19 +144,13 @@ export default function Header() {
                                 {hide ? (
                                     <>
                                         {" "}
-                                        <li
-                                            onClick={() => setHide(!hide)}
-                                            className=" px-5 py-2">
-                                            <Button className="bg-orange-500">
-                                                Huỷ
-                                            </Button>
+                                        <li onClick={() => setHide(!hide)} className=" px-5 py-2">
+                                            <Button className="bg-orange-500">Huỷ</Button>
                                         </li>
                                     </>
                                 ) : (
                                     <>
-                                        <li
-                                            onClick={() => setHide(!hide)}
-                                            className="block px-5 py-3">
+                                        <li onClick={() => setHide(!hide)} className="block px-5 py-3">
                                             <Search size={20}></Search>
                                         </li>
                                     </>
@@ -148,57 +167,27 @@ export default function Header() {
                                         <SheetTitle>Edit profile</SheetTitle>
                                         <ul className="text-left">
                                             <li className="">
-                                                <Link
-                                                    href="/"
-                                                    className={`${
-                                                        pathname === "/"
-                                                            ? "bg-orange-600"
-                                                            : ""
-                                                    } block px-5 py-3`}>
+                                                <Link href="/" className={`${pathname === "/" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                     Trang chủ
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link
-                                                    href="/phimbo"
-                                                    className={`${
-                                                        pathname === "/phimbo"
-                                                            ? "bg-orange-600"
-                                                            : ""
-                                                    } block px-5 py-3`}>
+                                                <Link href="/phimbo" className={`${pathname === "/phimbo" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                     Phim bộ
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link
-                                                    href="/phimle"
-                                                    className={`${
-                                                        pathname === "/phimle"
-                                                            ? "bg-orange-600"
-                                                            : ""
-                                                    } block px-5 py-3`}>
+                                                <Link href="/phimle" className={`${pathname === "/phimle" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                     Phim lẻ
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link
-                                                    href="/tvshow"
-                                                    className={`${
-                                                        pathname === "/tvshow"
-                                                            ? "bg-orange-600"
-                                                            : ""
-                                                    } block px-5 py-3`}>
+                                                <Link href="/tvshow" className={`${pathname === "/tvshow" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                     TV show
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link
-                                                    href="/hoathinh"
-                                                    className={`${
-                                                        pathname === "/hoathinh"
-                                                            ? "bg-orange-600"
-                                                            : ""
-                                                    } block px-5 py-3`}>
+                                                <Link href="/hoathinh" className={`${pathname === "/hoathinh" ? "bg-orange-600" : ""} block px-5 py-3`}>
                                                     Hoạt hình
                                                 </Link>
                                             </li>
