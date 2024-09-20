aa9da690af62df66460198deeb9e3cfe1cb15b83
Trong An <troandev@gmail.com>
2024-09-20 16:15:37 +0700

    add sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || "https://caphim.vercel.app/",
-    generateRobotsTxt: true, // (optional)
-    exclude: ["/sitemap.xml"], // <= exclude here
-    robotsTxtOptions: {
-        additionalSitemaps: ["https://caphim.vercel.app/sitemap.xml"],
-    },
-};
